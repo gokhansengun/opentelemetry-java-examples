103064e03c9af360a2ca7a4f6dec6a4f1306aa62
Gökhan Şengün <gokhansengun@gmail.com>
2024-08-29 02:33:03 +0300

    Enhance example with attributes

--- javaagent/src/main/java/io/opentelemetry/example/javagent/Controller.java
+++ javaagent/src/main/java/io/opentelemetry/example/javagent/Controller.java
@@ -8,6 +8,9 @@ import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,16 +35,44 @@ public class Controller {
     doWorkHistogram = meter.histogramBuilder("do-work").ofLongs().build();
   }
 
+  @GetMapping("/plain")
+  public String plain() throws InterruptedException {
+    int sleepTime = random.nextInt(200);
+
+    Thread.sleep(sleepTime);
+
+    return "plain";
+  }
+
   @GetMapping("/ping")
   public String ping() throws InterruptedException {
     int sleepTime = random.nextInt(200);
-    doWork(sleepTime);
+    doWork("doSingleWork", 0, sleepTime);
     doWorkHistogram.record(sleepTime, Attributes.of(ATTR_METHOD, "ping"));
     return "pong";
   }
 
-  private void doWork(int sleepTime) throws InterruptedException {
-    Span span = tracer.spanBuilder("doWork").startSpan();
+  @GetMapping("/zig")
+  public String zig() throws InterruptedException {
+    int sleepTime = random.nextInt(200);
+    int loopCount = 2 + random.nextInt(10);
+
+    for (int i = 0; i < loopCount; i++) {
+      doWork("doWorkAt-" + i, i, sleepTime);
+    }
+    return "zag";
+  }
+
+  private void doWork(String spanName, int loopCount, int sleepTime) throws InterruptedException {
+    Span span = tracer.spanBuilder(spanName).startSpan();
+
+    span.setAttribute("my-loop-count", loopCount);
+    span.setAttribute("my-random-val", random.nextInt(100));
+    // add day of week to the span
+    LocalDate today = LocalDate.now();
+    DayOfWeek dayOfWeek = today.getDayOfWeek();
+    span.setAttribute("my-day-of-week", dayOfWeek.toString());
+
     try (Scope ignored = span.makeCurrent()) {
       Thread.sleep(sleepTime);
       LOGGER.info("A sample log message!");
